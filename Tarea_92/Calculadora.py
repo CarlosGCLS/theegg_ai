90621a6bd23184df63c5a0b5f36fde9c17ed2fe8
CarlosGCLS <carloscls@gmail.com>
2022-10-26 20:14:05 +0200

    Tareas 92, 94 y 178

---
+++ Tarea_92/Calculadora.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def Suma (a,b):
+    return (a+b)
+
+def Resta (a,b):
+    return (a-b)
+
+def Multiplica (a,b):
+    return (a*b)
+
+def Divide (a,b):
+    return (a/b)
+
+
+
---
+++ tarea_94/Tiempo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def Tierra (Tiempo):
+    Altura = 0.5 * 9.8 * (Tiempo **2)
+    return (Altura)
+    
+def Marte (Tiempo):
+    Altura = 0.5 * 3.7 * (Tiempo **2)
+    return (Altura)
+
+def Jupiter (Tiempo):
+    Altura = 0.5 * 24.8 * (Tiempo **2)
+    return (Altura)
+
+
+# In[ ]:
+
+
+
+
